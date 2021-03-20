5452429e6ddfbb21054bdbfa2823227734d27afe
Тихонов Александр Анатольевич <Tikhonov_AA@stack-it.ru>
2021-03-20 12:06:50 +0300

    начало

---
+++ px_test/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <MainForm/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import MainForm from './components/mainForm.vue'
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    MainForm
+  }
+})
+</script>
---
+++ px_test/src/components/mainForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Тут будет чтото полезное</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+  }
+})
+</script>
