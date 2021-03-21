a802609898c5e643835bded689b5e1695ed13a0a
Тихонов Александр Анатольевич <Tikhonov_AA@stack-it.ru>
2021-03-22 01:33:08 +0300

    глобальный объект для доступа к апи ГХ

--- px_test/src/main.ts
+++ px_test/src/main.ts
@@ -1,5 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
+import {GitHubApi} from './clientAPI';
+
+// объект через который предлполагается взаимодействие с gitHub
+Vue.prototype.$http_gha = new GitHubApi;
 
 Vue.config.productionTip = false;
 
